1d6ab3901d688c7fab33a45ff51c1f39b12ef443
AnassEREKYSY <ereanass@gmail.com>
2024-04-16 12:11:49 +0200

    modification jest test 5

--- server.test.js
+++ server.test.js
@@ -1,7 +1,17 @@
 const request = require('supertest');
-const app = require('./server'); // Importez l'application Express depuis server.js
+const app = require('./server'); // Import your Express app
 
 describe('GET /', () => {
+  let server;
+
+  beforeAll(() => {
+    server = app.listen(3000); // Start the server
+  });
+
+  afterAll((done) => {
+    server.close(done); // Close the server connection
+  });
+
   it('responds with status 200 and a welcome message', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
