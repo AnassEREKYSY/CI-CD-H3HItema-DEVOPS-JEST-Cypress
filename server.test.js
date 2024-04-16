9531ae5414108b5c63c23a391c5bb1c1dd86a729
AnassEREKYSY <ereanass@gmail.com>
2024-04-16 11:57:37 +0200

    use jest

--- server.test.js
+++ server.test.js
@@ -3,7 +3,7 @@ const app = require('./server');
 
 describe('GET /', () => {
   it('responds with status 200 and a welcome message', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('http://localhost:3000');
     expect(response.status).toBe(200);
     expect(response.text).toEqual('Bienvenue sur notre serveur Express !');
   });
